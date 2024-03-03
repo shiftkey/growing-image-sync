ddfd4fb6145d6e137c9db79bede097f75f4f6b14
Brendan Forster <github@brendanforster.com>
2024-03-03 17:46:44 -0400

    add ImageSharp sample code for placing timestamp in image

--- UpdateLatestImageWithLabel.cs
+++ UpdateLatestImageWithLabel.cs
@@ -4,11 +4,13 @@ using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
 
 namespace Grow.Update
 {
-
-
     public class UpdateLatestImageWithLabel
     {
         private readonly ILogger _logger;
@@ -25,7 +27,7 @@ namespace Grow.Update
 
             var dateFileFormatRegex = new Regex(@"\d{4}\d{2}\d{2}-\d{2}\d{2}\d{2}");
             var containerName = "images";
-            //var latestFileName = "latest.jpg";
+            var latestFileName = "latest.jpg";
 
             FontCollection collection = new();
             var family = collection.Add("assets/NotoSansMono-Regular.ttf");
@@ -65,25 +67,93 @@ namespace Grow.Update
                 lastBlobClient.DownloadTo(initialStream);
                 initialStream.Seek(0, SeekOrigin.Begin);
 
-                // var streamForUploading = new MemoryStream();
+                var streamForUploading = new MemoryStream();
 
-                // using (Image img = Image.Load(initialStream))
-                // {
-                //     img.Mutate(ctx => ApplyScalingWaterMarkWordWrap(ctx, font, humanFriendlyDateTime, Color.White, 5));
-                //     img.Save(streamForUploading, new JpegEncoder());
-                // }
+                using (Image img = Image.Load(initialStream))
+                {
+                    img.Mutate(ctx => ApplyTimeWatermark(ctx, font, humanFriendlyDateTime, Color.White, 5));
+                    img.Save(streamForUploading, new JpegEncoder());
+                }
 
-                //streamForUploading.Seek(0, SeekOrigin.Begin);
+                streamForUploading.Seek(0, SeekOrigin.Begin);
 
-                // var latestBlobClient = new BlobClient(connectionString, containerName, latestFileName);
-                // latestBlobClient.Upload(streamForUploading, true);
+                var latestBlobClient = new BlobClient(connectionString, containerName, latestFileName);
+                latestBlobClient.Upload(streamForUploading, true);
             }
-
 
             if (myTimer.ScheduleStatus is not null)
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             }
+        }
+
+        // Source: https://github.com/SixLabors/Samples/blob/main/ImageSharp/DrawWaterMarkOnImage/Program.cs#L28
+        static IImageProcessingContext ApplyTimeWatermark(IImageProcessingContext processingContext,
+            Font font,
+            string text,
+            Color color,
+            float padding)
+        {
+            Size imgSize = processingContext.GetCurrentSize();
+            float targetWidth = imgSize.Width - (padding * 2);
+            float targetHeight = imgSize.Height - (padding * 2);
+
+            float targetMinHeight = imgSize.Height - (padding * 3); // Must be with in a margin width of the target height
+
+            // Now we are working in 2 dimensions at once and can't just scale because it will cause the text to
+            // reflow we need to just try multiple times
+            var scaledFont = font;
+            FontRectangle s = new FontRectangle(0, 0, float.MaxValue, float.MaxValue);
+
+            float scaleFactor = (scaledFont.Size / 2); // Every time we change direction we half this size
+            int trapCount = (int)scaledFont.Size * 2;
+            if (trapCount < 10)
+            {
+                trapCount = 10;
+            }
+
+            bool isTooSmall = false;
+
+            while ((s.Height > targetHeight || s.Height < targetMinHeight) && trapCount > 0)
+            {
+                if (s.Height > targetHeight)
+                {
+                    if (isTooSmall)
+                    {
+                        scaleFactor /= 2;
+                    }
+
+                    scaledFont = new Font(scaledFont, scaledFont.Size - scaleFactor);
+                    isTooSmall = false;
+                }
+
+                if (s.Height < targetMinHeight)
+                {
+                    if (!isTooSmall)
+                    {
+                        scaleFactor /= 2;
+                    }
+                    scaledFont = new Font(scaledFont, scaledFont.Size + scaleFactor);
+                    isTooSmall = true;
+                }
+                trapCount--;
+
+                s = TextMeasurer.MeasureSize(text, new TextOptions(scaledFont)
+                {
+                    WrappingLength = targetWidth
+                });
+
+            }
+
+            var center = new PointF(imgSize.Width, imgSize.Height);
+            var textOptions = new RichTextOptions(scaledFont)
+            {
+                Origin = center,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                WrappingLength = targetWidth
+            };
+            return processingContext.DrawText(textOptions, text, color);
         }
     }
 }
